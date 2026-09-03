314ad3da166c543eacf48916d39fe1c48007ba20
AleaScript <aleascript@proton.me>
2026-09-03 21:21:26 +0200

    Add custom design admonition type

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -93,6 +93,10 @@ const config: Config = {
           path: `./docs/${contentLocale}`,
           routeBasePath: '/',
           sidebarPath: './sidebars.ts',
+          admonitions: {
+            keywords: ['design'],
+            extendDefaults: true,
+          },
         },
         blog: false,
         pages: false,
